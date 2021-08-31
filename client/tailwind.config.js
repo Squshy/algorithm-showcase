cb8c5b00f0c8ab4b7f7289e51af87440c94fe9db
Unknown <Calvin.lapp@dcmail.ca>
2021-08-31 12:47:36 -0400

    nav starter

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,30 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        // gray: {
+        //   50: "#f5f5f5",
+        //   100: "#EDEDED",
+        //   200: "#DBDBDB",
+        //   300: "#C2C2C2",
+        //   400: "#A6A6A6",
+        //   500: "#858585",
+        //   600: "#666666",
+        //   700: "#4F4F4F",
+        //   800: "#454545",
+        //   900: "#383838",
+        //   1100: "#1e1e1e",
+        // },
+      },
+      maxWidth: {
+        '8xl': "92rem"
+      }
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
