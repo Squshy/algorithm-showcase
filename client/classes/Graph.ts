98ad9852228fb5707661ceb84efd7664280d2de7
Unknown <Calvin.lapp@dcmail.ca>
2021-09-02 12:40:42 -0400

    changed to set instead of array

--- client/classes/Graph.ts
+++ client/classes/Graph.ts
@@ -2,37 +2,58 @@ import { Heap } from "./Heap";
 import { Node } from "./Node";
 
 export class Graph {
-  numVertices:number;
-  adjacenyList:Map<string, Array<Node>>;
+  numRows: number;
+  numCols: number;
+  adjacenyList: Map<string, Set<Node>>;
 
   constructor(numRows: number, numCols: number) {
-    this.numVertices = numRows * numCols;
+    this.numRows = numRows;
+    this.numCols = numCols;
     this.adjacenyList = new Map();
   }
 
   // add vertex v as key to adjaceny list
-  addVertex(v:Node) {
-    this.adjacenyList.set(v.getRowCol(), []);
+  addVertex(v: Node) {
+    this.adjacenyList.set(v.getRowCol(), new Set());
   }
 
-  addEdge(v:Node, w:Node) {
+  addEdge(v: Node, w: Node) {
+    const vSet = this.adjacenyList.get(v.getRowCol());
+    const wSet = this.adjacenyList.get(w.getRowCol());
+
     // set edge between v and w
-    this.adjacenyList.get(v.getRowCol())!.push(w);
+    if (!vSet?.has(w)) {
+      this.adjacenyList.get(v.getRowCol())!.add(w);
+    } else {
+      console.log(`${w} is already in the set v`)
+    }
+
     // undirected graph so do the same with w to v
-    this.adjacenyList.get(w.getRowCol())!.push(v);
+    if (!wSet?.has(v)) {
+      this.adjacenyList.get(w.getRowCol())!.add(v);
+    }
+  }
+
+  addNeighbouringEdgesToNode(node: Node, row: number, col: number) {
+    const nodeToRowCol = (row: number, col: number) => {
+      return `${row} ${col}`;
+    };
+
+    if (node.row > 0) {
+    }
   }
 
   printGraph() {
     const keys = this.adjacenyList.keys();
-    
-    for(let key of Array.from(keys)) {
+
+    for (let key of Array.from(keys)) {
       const currentValue = this.adjacenyList.get(key);
       let string = "";
 
-      for(let j of currentValue!) {
-        string += `[${j.getRowCol()}] `;
-      }
-      console.log(key + " -> " + string)
+      currentValue?.forEach((node) => {
+        string += `[${node.getRowCol()}]`;
+      });
+      console.log(key + " -> " + string);
     }
   }
 }
--- client/components/grid/Grid.tsx
+++ client/components/grid/Grid.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { useRef } from "react";
 import { useState } from "react";
+import { Graph } from "../../classes/Graph";
+import { Node } from "../../classes/Node";
 import { useOnScreenResize } from "../../hooks/useOnResize";
 import { SpinnerIcon } from "../../svg/Spinner";
 import { GridNode } from "./GridNode";
@@ -22,6 +24,7 @@ export const Grid: React.FC<GridProps> = ({}) => {
   });
   const gridRef: any = useRef<HTMLDivElement>();
   const [loadingGrid, setLoadingGrid] = useState<boolean>(true);
+  const [graph, setGraph] = useState<Graph | undefined>();
 
   useEffect(() => {
     setLoadingGrid(true)
@@ -43,24 +46,19 @@ export const Grid: React.FC<GridProps> = ({}) => {
     }, 250);
   }, [width, height]);
 
-  const createGrid = (): Array<JSX.Element> => {
-    const nodes: Array<JSX.Element> = [];
+  const createGrid = () => {
+    // const nodes: Array<JSX.Element> = [];
+    const newGraph = new Graph(gridDimensions.rows, gridDimensions.cols);
 
     for (let i = 0; i < gridDimensions.rows; i++) {
       for (let j = 0; j < gridDimensions.cols; j++) {
-        nodes.push(
-          <GridNode
-            key={`${i}-${j}`}
-            row={i}
-            col={j}
-            endRow={i === gridDimensions.rows - 1}
-            endCol={j === gridDimensions.cols - 1}
-          />
-        );
+        const newNode = new Node(i, j);
+        newGraph.addVertex(newNode)
       }
     }
 
-    return nodes;
+    setGraph(newGraph);
+    // return nodes;
   };
 
   return (
@@ -68,10 +66,10 @@ export const Grid: React.FC<GridProps> = ({}) => {
       className={`w-full px-2 py-3 border-gray-800 border-2 rounded-md bg-gray-700 bg-opacity-25`}
     >
       <div
-        className={`w-full flex flex-wrap h-64 justify-center`}
+        className={`w-full flex flex-wrap h-96 justify-center`}
         ref={gridRef}
       >
-        {loadingGrid ? <SpinnerIcon className={`h-12 w-12 text-gray-600 self-center`} /> : createGrid()}
+        {/* {loadingGrid ? <SpinnerIcon className={`h-12 w-12 text-gray-600 self-center`} /> : createGrid()} */}
       </div>
     </div>
   );
