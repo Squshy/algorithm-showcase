c0edeba4f262f1ad2cce51bb5143a7042e22913f
Unknown <Calvin.lapp@dcmail.ca>
2021-09-08 20:26:33 -0400

    simple about done

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next";
 import { MainBody } from "../components/MainBody";
 import { Nav } from "../components/nav/Nav";
-import Link from "next/link";
 import { useSetLink } from "../hooks/useSetLink";
 import { SIDEBAR_LINKS } from "../constants";
 
@@ -12,6 +11,30 @@ const Home: NextPage = () => {
       <Nav />
       <MainBody>
         <h1 className={`text-3xl font-extrabold`}>About this site</h1>
+        <div className={`mt-6 space-y-4`}>
+          <p>
+            This site is designed to showcase different algorithms and to
+            visually see how they work. This can range from path finding
+            algorithms, to sorting algorithms.
+          </p>
+          <p className={`hidden lg:block`}>
+            To see the different algorithms offered, check out the side bar!
+          </p>
+          <p className={`block lg:hidden`}>
+            To see the different algorithms offered, check out the button in the
+            bottom right of your screen!
+          </p>
+          <p>
+            This code is available on{" "}
+            <a
+              href="https://github.com/Squshy/algorithm-showcase"
+              className={`transition duration-100 ease-in-out text-purple-400 hover:text-purple-500`}
+            >
+              GitHub
+            </a>
+            .
+          </p>
+        </div>
       </MainBody>
     </>
   );
